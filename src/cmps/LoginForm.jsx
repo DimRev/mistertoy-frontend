2cf7e26c95ae80a9a995acd8092f5c6082146284
DimRev <dimarev444@gmail.com>
2023-12-26 20:06:27 +0200

    Edit: Added MUI inputs

--- src/cmps/LoginForm.jsx
+++ src/cmps/LoginForm.jsx
@@ -1,52 +1,57 @@
-import { useState } from "react"
-import { userService } from "../services/user.service.js"
-import { Button } from "@mui/material"
+import { useState } from 'react'
+import { Button, Input } from '@mui/material'
+import { userService } from '../services/user.service.js'
 
 // const { useState } = React
 
 export function LoginForm({ onLogin, isSignup }) {
+  const [credentials, setCredentials] = useState(
+    userService.getEmptyCredentials()
+  )
 
-    const [credentials, setCredentials] = useState(userService.getEmptyCredentials())
+  function handleChange({ target }) {
+    const { name: field, value } = target
+    setCredentials((prevCreds) => ({ ...prevCreds, [field]: value }))
+  }
 
-    function handleChange({ target }) {
-        const { name: field, value } = target
-        setCredentials(prevCreds => ({ ...prevCreds, [field]: value }))
-    }
+  function handleSubmit(ev) {
+    ev.preventDefault()
+    onLogin(credentials)
+  }
 
-    function handleSubmit(ev) {
-        ev.preventDefault()
-        onLogin(credentials)
-    }
-
-    return (
-        <form className="login-form" onSubmit={handleSubmit}>
-            <input
-                type="text"
-                name="username"
-                value={credentials.username}
-                placeholder="Username"
-                onChange={handleChange}
-                required
-                autoFocus
-            />
-            <input
-                type="password"
-                name="password"
-                value={credentials.password}
-                placeholder="Password"
-                onChange={handleChange}
-                required
-                autoComplete="off"
-            />
-            {isSignup && <input
-                type="text"
-                name="fullname"
-                value={credentials.fullname}
-                placeholder="Full name"
-                onChange={handleChange}
-                required
-            />}
-            <Button variant="contained">{isSignup ? 'Signup' : 'Login'}</Button>
-        </form>
-    )
+  return (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <Input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={credentials.username}
+        onChange={handleChange}
+        required
+        autoFocus
+      />
+      <Input
+        type="password"
+        name="password"
+        value={credentials.password}
+        placeholder="Password"
+        onChange={handleChange}
+        required
+        autoComplete="off"
+      />
+      {isSignup && (
+        <Input
+          type="text"
+          name="fullname"
+          value={credentials.fullname}
+          placeholder="Full name"
+          onChange={handleChange}
+          required
+        />
+      )}
+      <Button variant="contained" onClick={handleSubmit}>
+        {isSignup ? 'Signup' : 'Login'}
+      </Button>
+    </form>
+  )
 }
