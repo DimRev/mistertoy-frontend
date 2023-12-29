1b428aac7362b23d25d446d1c0526550d5f385b8
DimRev <dimarev444@gmail.com>
2023-12-29 15:39:10 +0200

    Edit: Improved the custom hook, can now redirect admin/user to path

--- hooks/useAdminRedirect.jsx
+++ hooks/useAdminRedirect.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-const useAdminRedirect = (redirect = true) => {
+const useAdminRedirect = (redirect = true,unAuthPath = '/', authPath = '/') => {
   const user = useSelector((storeState) => storeState.userModule.loggedinUser)
 
   if (redirect) {
@@ -10,7 +10,9 @@ const useAdminRedirect = (redirect = true) => {
 
     useEffect(() => {
       if (!user?.isAdmin) {
-        navigate('/')
+        navigate(unAuthPath)
+      } else if (user.isAdmin){
+        navigate(authPath)
       }
     }, [user, navigate])
   }
